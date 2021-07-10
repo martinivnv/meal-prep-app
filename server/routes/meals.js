f4cb85681c3805d9a8a63b50a6e222e00a25890a
martinivnv <martinivnv2002@gmail.com>
2021-07-10 13:38:49 -0400

    Created meal model

---
+++ server/routes/meals.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+
+let Meal = require("../models/meal.model");
+
+// READ all meals
+router.route("/meal").get((req, res) => {
+	Meal.find()
+		.then((meals) => meals.json())
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+
+// CREATE a new meal
+router.route("/add").post((req, res) => {
+	const name = req.body.name;
+	const portions = req.body.portions;
+	const cost = Number(req.body.cost);
+	const type = req.body.type;
+	const date = Date.parse(req.body.date);
+
+	const newMeal = new Meal({ name, portions, cost, type, date });
+
+	newMeal
+		.save()
+		.then(() => res.json("Meal added!"))
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+
+// READ a meal by its ID
+router.route("/:id").get((req, res) => {
+	Meal.findById(req.params.id)
+		.then((meal) => res.json(meal))
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+
+// DELETE a meal by its ID
+router.route("/:id").delete((req, res) => {
+	Meal.findByIdAndDelete(req.params.id)
+		.then(() => res.json("Meal deleted."))
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+
+// UPDATE a meal by its ID
+router.route("/update/:id").post((req, res) => {
+	Meal.findById(req.params.id)
+		.then((meal) => {
+			meal.name = req.body.name;
+			meal.portions = req.body.portions;
+			meal.cost = Number(req.body.cost);
+			meal.type = req.body.type;
+			meal.date = Date.parse(req.body.date);
+
+			meal
+				.save()
+				.then(() => res.json("Meal updated!"))
+				.catch((err) => res.status(400).json("Error: " + err));
+		})
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
