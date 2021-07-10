2eb2ac1ea583374cda057ca8c01104197948d130
martinivnv <martinivnv2002@gmail.com>
2021-07-10 13:39:06 -0400

    Created meal model

---
+++ server/models/meal.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const mealSchema = new Schema({
+	name: { type: String, required: true },
+	portions: { type: Number, required: true },
+	cost: { type: Number, required: true, min: 1, max: 3 }, // i.e. "$", "$$", or "$$$"
+	type: { type: String, required: true }, // Home-cooked, delivery, dining out
+	date: { type: Date, required: true },
+});
+
+const Meal = mongoose.model("Meal", mealSchema);
+
+module.exports = Meal;
