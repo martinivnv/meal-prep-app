f287817395e03c6c51bab39a6b3bcbd6562c8ce9
martinivnv <martinivnv2002@gmail.com>
2021-07-11 14:16:28 -0400

    Add new components to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+
+import NavBar from "./components/NavBar";
+import CalendarGrid from "./components/CalendarGrid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="container">
+				<NavBar />
+				<CalendarGrid />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
