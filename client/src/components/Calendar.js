54a4c3acbd8168550cfb78f2c9ac1e82e07329c5
martinivnv <martinivnv2002@gmail.com>
2021-07-13 15:34:33 -0400

    Customized fullCalendar component

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,15 +1,16 @@
-import React from "react";
+import { React } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
+// import interactionPlugin from "@fullcalendar/interaction";
 import "./Calendar.css";
 
-const Calendar = () => {
+const Calendar = (props) => {
 	return (
 		<FullCalendar
 			plugins={[dayGridPlugin]}
 			initialView="dayGridMonth"
 			headerToolbar={false}
+			height={"92vh"}
 		/>
 	);
 };
