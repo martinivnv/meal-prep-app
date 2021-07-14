9c8ff5faf3bb84086e1cf31571c69a2f5d616270
martinivnv <martinivnv2002@gmail.com>
2021-07-14 15:58:28 -0400

    Add modal pop up for adding meals

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,11 +1,14 @@
-import { React, useRef, useEffect } from "react";
+import { React, useRef, useEffect, useState, Fragment } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-// import interactionPlugin from "@fullcalendar/interaction";
+import interactionPlugin from "@fullcalendar/interaction";
 import "./Calendar.css";
+import Modal from "@material-ui/core/Modal";
+import TextField from "@material-ui/core/TextField";
 
 const Calendar = (props) => {
 	const calendarRef = useRef(null);
+	const [modalOpen, setModalOpen] = useState(false);
 
 	useEffect(() => {
 		let calendarApi = calendarRef.current.getApi();
@@ -13,14 +16,55 @@ const Calendar = (props) => {
 		calendarApi.gotoDate(date);
 	}, [props.month, props.year]);
 
+	const handleOpen = () => setModalOpen(true);
+
+	const handleClose = () => setModalOpen(false);
+
+	const handleSave = () => {};
+
+	const [body, setBody] = useState({
+		name: "",
+		portions: 1,
+		cost: 1,
+		type: "",
+		date: Date.now(),
+	});
+
+	const modalForm = (
+		<div class="modalContent">
+			<h2 id="modalTitle">Add A Meal</h2>
+			<form className="modalForm" noValidate autoComplete="off">
+				<TextField id="meal-name" label="Meal Name" required />
+			</form>
+		</div>
+	);
+
 	return (
-		<FullCalendar
-			plugins={[dayGridPlugin]}
-			initialView="dayGridMonth"
-			headerToolbar={false}
-			height={"92vh"}
-			ref={calendarRef}
-		/>
+		<Fragment>
+			<FullCalendar
+				plugins={[dayGridPlugin, interactionPlugin]}
+				initialView="dayGridMonth"
+				headerToolbar={false}
+				height={"92vh"}
+				ref={calendarRef}
+				dateClick={(info) => {
+					setBody({
+						...body,
+						date: info.date,
+					});
+					handleOpen();
+				}}
+			/>
+			<Modal
+				open={modalOpen}
+				onClose={handleClose}
+				className="mealModal"
+				aria-labelledby="meal-modal"
+				aria-describedby="add-edit-delete-meals"
+			>
+				{modalForm}
+			</Modal>
+		</Fragment>
 	);
 };
 
